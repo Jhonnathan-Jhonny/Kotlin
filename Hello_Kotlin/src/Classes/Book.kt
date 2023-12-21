6bed028ca99c430d9425ab65cec87b08ceb52dea
Jhonnathan-Jhonny <jhonnathan.rodrigues@aluno.uepb.edu.br>
2023-12-21 07:42:33 -0300

    praticando Herança com exemplo de livro

--- Hello_Kotlin/src/Classes/Book.kt
+++ Hello_Kotlin/src/Classes/Book.kt
@@ -1,5 +1,20 @@
 package Classes
 
-class Book {
-    
+import java.awt.print.Book
+
+open class Book (var titulo: String, var autor: String){
+
+    open private var paginaAtual: Int = 0
+    open fun lerPagina(){
+
+        paginaAtual++
+    }
+}
+
+class eBook(val formato: String = "text"): Book() {
+    override var palavraAtual: Int = 0
+    override  fun lerPagina(){
+        palavraAtual += 250 //Cada página tem 250 palavras
+    }
+
 }
