1e9bd833af2b5b01b7d2e4f1800d8822bb7818ab
Jhonnathan-Jhonny <jhonnathan.rodrigues@aluno.uepb.edu.br>
2023-12-21 07:22:09 -0300

    Estudando conceitos de herança em kotlin

--- Aquarium/src/Aquarium/Aquarium.kt
+++ Aquarium/src/Aquarium/Aquarium.kt
@@ -1,13 +1,13 @@
 package Aquarium
 
-class Aquarium (var width: Int = 20,var heigth: Int = 40, var length: Int = 100) {
+open class Aquarium (var width: Int = 20,var heigth: Int = 40, var length: Int = 100) {
     var volume : Int
         get() = width * heigth * length / 1000
         //No set pega um volume(value) e calcula a nova altura do tanque
         set(value) {heigth = (value*1000)/(width*length)}
     //Pegou o "value" em litros, transformou para cm^3 e atribuiu a altura
 
-    var water = volume * 0.9 //Quantidade de água permitida
+    open var water = volume * 0.9 //Quantidade de água permitida
     //Construtor secundário (Muito pouco utilizado)
     constructor(numberOfFish: Int): this(width = 11){
         //Calculos expecificos do construtor
@@ -16,4 +16,9 @@ class Aquarium (var width: Int = 20,var heigth: Int = 40, var length: Int = 100)
         val tank: Double = water + water * 0.1
         heigth = (tank/(length*width)).toInt()
     }
+}
+
+//Herança
+class towerTank(): Aquarium(){
+    override var water = volume * 0.8
 }
